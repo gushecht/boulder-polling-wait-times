5542c7bcbe396622891a47f329493bea48267c2d
Gus Hecht <gushecht@gmail.com>
2020-10-16 11:20:41 -0700

    Reorder routes

--- src/server.js
+++ src/server.js
@@ -6,14 +6,14 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
-});
-
 app.get('/wait-times', (req, res) => {
   fetch('https://app.redash.io/boulder-county/api/queries/492531/results.json?api_key=inMwpXG9rggvAE4TLYN8Th6sQrYCnl2Z56NRp7aV')
     .then(res => res.json())
     .then(json => res.send(json.query_result.data.rows));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
