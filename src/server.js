f5b0c096bea63ed8b1efb64a7433b168a0185bee
Gus Hecht <gushecht@gmail.com>
2020-10-16 11:48:14 -0700

    logging

--- src/server.js
+++ src/server.js
@@ -7,8 +7,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('/wait-times', async (req, res) => {
+  console.log('wait times')
   const response = await fetch('https://app.redash.io/boulder-county/api/queries/492531/results.json?api_key=inMwpXG9rggvAE4TLYN8Th6sQrYCnl2Z56NRp7aV')
+  console.log(response)
   const json = await response.json();
+  console.log(json)
   res.send(json.query_result.data.rows);
 });
 
