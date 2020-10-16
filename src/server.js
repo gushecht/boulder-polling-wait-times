5fb3c19e2060c28178a96b66c5dd2447e32fd24c
Gus Hecht <gushecht@gmail.com>
2020-10-16 11:27:29 -0700

    Another proxy attempt

--- src/server.js
+++ src/server.js
@@ -6,10 +6,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get('/wait-times', (req, res) => {
-  fetch('https://app.redash.io/boulder-county/api/queries/492531/results.json?api_key=inMwpXG9rggvAE4TLYN8Th6sQrYCnl2Z56NRp7aV')
-    .then(res => res.json())
-    .then(json => res.send(json.query_result.data.rows));
+app.get('/wait-times', async (req, res) => {
+  const response = await fetch('https://app.redash.io/boulder-county/api/queries/492531/results.json?api_key=inMwpXG9rggvAE4TLYN8Th6sQrYCnl2Z56NRp7aV')
+  const json = await response.json();
+  res.send(json.query_result.data.rows);
 });
 
 app.get('/', (req, res) => {
@@ -17,3 +17,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(process.env.PORT || 8080);
+
+console.log(`Listening at ${process.env.PORT || 8080}`);
