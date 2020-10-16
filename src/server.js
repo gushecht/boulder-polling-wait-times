d8bc5c518ffde89f0199703e835a97ca813e1685
Gus Hecht <gushecht@gmail.com>
2020-10-16 11:51:44 -0700

    Remove logging

--- src/server.js
+++ src/server.js
@@ -6,13 +6,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get('/wait-times', async (req, res) => {
-  console.log('wait times')
-  const response = await fetch('https://app.redash.io/boulder-county/api/queries/492531/results.json?api_key=inMwpXG9rggvAE4TLYN8Th6sQrYCnl2Z56NRp7aV')
-  console.log(response)
-  const json = await response.json();
-  console.log(json)
-  res.send(json.query_result.data.rows);
+app.get('/wait-times', (req, res) => {
+  fetch('https://app.redash.io/boulder-county/api/queries/492531/results.json?api_key=inMwpXG9rggvAE4TLYN8Th6sQrYCnl2Z56NRp7aV')
+    .then(res => res.json())
+    .then(json => res.send(json.query_result.data.rows));
 });
 
 app.get('/', (req, res) => {
